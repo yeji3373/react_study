f3ed70a7a667c17137010796390ed48f34e8456b
Yeji Lee <fpdlsqhdnvj@naver.com>
2024-09-03 17:41:03 +0900

    react composition and context studying

--- src/index.js
+++ src/index.js
@@ -6,11 +6,16 @@ import reportWebVitals from './reportWebVitals';
 import Accommodate from './chapter_07/Accommodate';
 import ConfirmButton from './chapter_08/ConfirmButton';
 import LandingPage from './chapter_09/LandingPage';
+import AttendanceBook from './chapter_10/AttendanceBook';
+import SignUp from './chapter_11/SignUp';
+import Calculator from './chapter_12/Calculator';
+import ProfileCard from './chapter_13/ProfileCard';
+import DarkOrLight from './chapter_14/DarkOrLight';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <LandingPage />
+    <DarkOrLight />
   </React.StrictMode>
 );
 
